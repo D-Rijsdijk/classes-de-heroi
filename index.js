d9f9af29c4b42ee42566f3abca3d90cb010eb580
D-Rijsdijk <diana.rijsdijk09@gmail.com>
2023-12-07 19:29:25 -0400

    Adaptado com a função perfil

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ class herosSheet {
 
         console.log("O " +this.herosType+ " atacou usando " +herosAttack);
     }
+
+    perfil(){
+        console.log("-------------------------------------");
+        console.log("O nome do herói é: " +this.herosName);
+        console.log("Sua idade é: " +this.herosAge);
+        console.log("Seu tipo é: " +this.herosType);
+        console.log("-------------------------------------");
+    }
 }
 
 let hero01 = new herosSheet("Pato Donald", 25, "mago");
@@ -35,7 +43,12 @@ let hero02 = new herosSheet("Huguinho", 13, "guerreiro");
 let hero03 = new herosSheet("Zézinho", 13, "monge");
 let hero04 = new herosSheet("Luisinho", 13, "ninja");
 
-hero01.attack()
-hero02.attack()
-hero03.attack()
-hero04.attack()
+hero01.attack();
+hero02.attack();
+hero03.attack();
+hero04.attack();
+
+hero01.perfil();
+hero02.perfil();
+hero03.perfil();
+hero04.perfil();
