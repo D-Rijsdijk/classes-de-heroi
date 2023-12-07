359a3c431c333eb75cf9524b1909b9c8e698bb42
D-Rijsdijk <diana.rijsdijk09@gmail.com>
2023-12-07 17:23:41 -0400

    classe criada

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-class
+class hero {
+    constructor(herosName, herosAge, herosType){
+        this.herosName = herosName;
+        this.herosAge = herosAge;
+        this.herosType = herosType;
+    }
+
+}
