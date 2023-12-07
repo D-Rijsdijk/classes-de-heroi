c5f3a9d09d60ffa7cfecc3d58538b0695817845c
D-Rijsdijk <diana.rijsdijk09@gmail.com>
2023-12-07 19:17:48 -0400

    desafio completo

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-class hero {
+class herosSheet {
     constructor(herosName, herosAge, herosType){
         this.herosName = herosName;
         this.herosAge = herosAge;
         this.herosType = herosType;
     }
+    
+    attack(){
+        let herosAttack;
 
+        switch(this.herosType){
+            case "mago":
+               herosAttack = "magia";
+               break;
+               
+            case "guerreiro":
+                herosAttack = "espada";
+                break;
+
+            case "monge":
+                herosAttack = "artes marciais";
+                break;
+
+            case "ninja":
+                herosAttack = "shuriken";
+                break;
+        }
+
+        console.log("O " +this.herosType+ " atacou usando " +herosAttack);
+    }
 }
+
+let hero01 = new herosSheet("Pato Donald", 25, "mago");
+let hero02 = new herosSheet("Huguinho", 13, "guerreiro");
+let hero03 = new herosSheet("Zézinho", 13, "monge");
+let hero04 = new herosSheet("Luisinho", 13, "ninja");
+
+hero01.attack()
+hero02.attack()
+hero03.attack()
+hero04.attack()
